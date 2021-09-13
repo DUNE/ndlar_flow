93d54133c923a28b6ef6e53ebf2d0d48274ef00a
peter-madigan <pmadigan@berkeley.edu>
2021-09-13 12:26:44 -0700

    Fix typo

--- h5flow_modules/misc/broken_track_sim.py
+++ h5flow_modules/misc/broken_track_sim.py
@@ -121,7 +121,7 @@ class BrokenTrackSim(H5FlowStage):
 
         self.generate_2track_joint_pdf = params.get('generate_2track_joint_pdf', True)
         self.joint_pdf_filename = params.get('joint_pdf_filename',
-                                             f'joint_pdf-{self.class_version.replace('.','_')}.npz')
+                                             f'joint_pdf-{self.class_version.replace(".","_")}.npz')
         self.pdf_bins = [np.logspace(*bins) for bins in params.get('pdf_bins',
                                                                    self.default_pdf_bins)]
         self.pdf = dict()
