447ed13b57768d7ca806da5e30abc9ad55b6c132
peter-madigan <pmadigan@berkeley.edu>
2021-09-14 16:41:23 -0700

    Refine stopping muon selection

--- h5flow_modules/analysis/stopping_muon_selection.py
+++ h5flow_modules/analysis/stopping_muon_selection.py
@@ -23,8 +23,11 @@ class StoppingMuonSelection(H5FlowStage):
     '''
     class_version = '0.0.0'
 
-    default_fid_cut = 25  # mm
+    default_fid_cut = 20 # mm
+    default_cathode_fid_cut = 20 # mm
+    default_length_cut = 100 # mm
     default_merged_dset_name = 'combined/tracklets/merged'
+    default_t0_dset_name = 'combined/t0'
     default_mc_trajectory_path = ['combined/tracklets/merged',
                                   'charge/hits', 'charge/packets',
                                   'mc_truth/tracks',
@@ -44,6 +47,10 @@ class StoppingMuonSelection(H5FlowStage):
 
         self.path = params.get('path', self.default_path)
         self.fid_cut = params.get('fid_cut', self.default_fid_cut)
+        self.cathode_fid_cut = params.get('cathode_fid_cut', self.default_cathode_fid_cut)        
+        self.length_cut = params.get('fid_cut', self.default_length_cut)        
+        self.t0_dset_name = params.get('t0_dset_name',
+                                       self.default_t0_dset_name)
         self.merged_dset_name = params.get('merged_dset_name',
                                            self.default_merged_dset_name)
         self.mc_trajectory_path = params.get('mc_trajectory_path',
@@ -58,6 +65,7 @@ class StoppingMuonSelection(H5FlowStage):
                                     classname=self.classname,
                                     class_version=self.class_version,
                                     fid_cut=self.fid_cut,
+                                    length_cut=self.length_cut,
                                     merged_dset_name=self.merged_dset_name
                                     )
         self.data_manager.create_dset(f'{self.path}/{self.event_sel_dset_name}',
@@ -71,19 +79,22 @@ class StoppingMuonSelection(H5FlowStage):
                                           self.track_sel_dtype)
 
         self.create_regions()
+        self.data_manager.set_attrs(self.path, regions=np.array(self.regions))
 
     def create_regions(self):
         x = resources['Geometry'].pixel_xy.compress((0,))
         y = resources['Geometry'].pixel_xy.compress((1,))
         z = resources['Geometry'].anode_z.compress()
 
-        min_x, max_x = np.min(x), np.max(x)
-        min_y, max_y = np.min(y), np.max(y)
-        min_z, max_z = np.min(z), np.max(z)
+        min_x, max_x = np.min(x) + self.fid_cut, np.max(x) - self.fid_cut
+        min_y, max_y = np.min(y) + self.fid_cut, np.max(y) - self.fid_cut
+        min_z, max_z = np.min(z) + self.fid_cut, np.max(z) - self.fid_cut
 
         self.regions.append((np.array([min_x, min_y, min_z]),
+                             np.array([max_x, max_y, -self.cathode_fid_cut])))
+        self.regions.append((np.array([min_x, min_y, self.cathode_fid_cut]),
                              np.array([max_x, max_y, max_z])))
-
+        
     def in_fid(self, xyz):
         '''
             :param xyz: array ``shape: (N,3)``
@@ -91,10 +102,12 @@ class StoppingMuonSelection(H5FlowStage):
             :returns: array ``shape: (N,)``
 
         '''
-        in_a_fid = ma.all([(xyz < np.expand_dims(boundary[1], 0))
-                           & (xyz > np.expand_dims(boundary[0], 0))
-                           for boundary in self.regions], axis=-1)
-        return ma.any(in_a_fid, axis=0)
+        coord_in_fid = ma.concatenate([np.expand_dims((xyz < np.expand_dims(boundary[1], 0))
+                                       & (xyz > np.expand_dims(boundary[0], 0)), axis=-1)
+                                       for boundary in self.regions], axis=-1)
+        in_fid = ma.all(coord_in_fid, axis=1)
+        in_any_fid = ma.any(in_fid, axis=-1)
+        return in_any_fid
 
     def contained(self, start_xyz, end_xyz):
         '''
@@ -105,8 +118,21 @@ class StoppingMuonSelection(H5FlowStage):
             :returns: array ``shape: (N,)``
 
         '''
-        return ((self.in_fid(start_xyz) & ~self.in_fid(end_xyz))
-                | (~self.in_fid(start_xyz) & self.in_fid(end_xyz)))
+        start_in_fid = self.in_fid(start_xyz)
+        end_in_fid = self.in_fid(end_xyz)
+        track_contained = (~start_in_fid & end_in_fid) | (start_in_fid & ~end_in_fid)
+        return track_contained
+
+    def through_going(self, start_xyz, end_xyz):
+        start_in_fid = self.in_fid(start_xyz)
+        end_in_fid = self.in_fid(end_xyz)
+        track_through_going = ~start_in_fid & ~end_in_fid
+        return track_through_going
+
+    def downward(self, start_xyz, end_xyz):
+        end_in_fid = self.in_fid(end_xyz)
+        dy = end_xyz[:,1] - start_xyz[:,1]
+        return ((end_in_fid & (dy < 0)) | (~end_in_fid & (dy > 0)))
 
     def load_track_trajectories(self, source_name, source_slice, path):
         chain = list(zip([source_name] + path[:-1], path))
@@ -119,11 +145,19 @@ class StoppingMuonSelection(H5FlowStage):
 
     def run(self, source_name, source_slice, cache):
         tracks = cache[self.merged_dset_name]
+        t0 = cache[self.t0_dset_name].reshape(cache[source_name].shape)
 
         # find all tracks that end in the fiducial volume
-        is_stopping = self.contained(tracks.ravel()['start'],
-                                     tracks.ravel()['end'])
+        is_stopping = (self.contained(tracks.ravel()['start'],
+                                      tracks.ravel()['end'])
+                       & (tracks.ravel()['length'] > self.length_cut))
+        is_throughgoing = (self.through_going(tracks.ravel()['start'],
+                                              tracks.ravel()['end']))
+        is_downward = self.downward(tracks.ravel()['start'],
+                                    tracks.ravel()['end'])
         is_stopping = is_stopping.reshape(tracks.shape)
+        is_throughgoing = is_throughgoing.reshape(tracks.shape)
+        is_downward = is_downward.reshape(tracks.shape)
 
         if self.is_mc:
             # lookup the track's true trajectory
@@ -139,30 +173,44 @@ class StoppingMuonSelection(H5FlowStage):
             track_true_traj = track_true_traj.reshape(tracks.shape)
 
             # find if trajectory ends in the fiducial volume
-            is_true_stopping = self.contained(track_true_traj.ravel()['xyz_start'],
-                                              track_true_traj.ravel()['xyz_end'])
+            xyz_start = track_true_traj.ravel()['xyz_start'].copy()
+            xyz_end = track_true_traj.ravel()['xyz_end'].copy()
+            # FIXME: coordinates are weird between sim and data
+            xyz_start[:,1] += 218.236
+            xyz_end[:,1] += 218.236
+            new_x_start = xyz_start[:,2].copy()
+            new_x_end = xyz_end[:,2].copy()
+            new_z_start = xyz_start[:,0].copy()
+            new_z_end = xyz_end[:,0].copy()
+            xyz_start[:,0] = new_x_start
+            xyz_start[:,2] = new_z_start
+            xyz_end[:,0] = new_x_end
+            xyz_end[:,2] = new_z_end
+            is_muon = ma.abs(track_true_traj['pdgId']) == 13
+            is_true_stopping = self.contained(xyz_start, xyz_end)
             is_true_stopping = is_true_stopping.reshape(tracks.shape)
 
         # define a stopping event as one with exclusively 1 track that ends in fid.
-        event_is_stopping = np.count_nonzero(is_stopping, axis=-1) == 1
+        event_is_stopping = ((ma.sum(is_stopping, axis=-1) == 1)
+                             & (t0['type'] != 0)
+                             & (ma.sum(is_throughgoing, axis=-1) == 0)
+                             & (ma.sum(is_stopping & is_downward, axis=-1) == 1))
 
         if self.is_mc:
             # define true stopping events as events with at least 1 muon that ends in fid.
-            is_muon = np.abs(track_true_traj['pdgId']) == 13
-            event_is_true_stopping = np.count_nonzero(is_muon
-                                                      & is_true_stopping,
-                                                      axis=-1) >= 1
+            event_is_true_stopping = ma.sum(is_muon & is_true_stopping, axis=-1) >= 1
 
         # prep arrays to write to file
-        event_sel = np.empty(len(tracks), dtype=self.event_sel_dtype)
-        track_sel = np.empty(len(tracks['id'].compressed()), dtype=self.track_sel_dtype)
+        event_sel = np.zeros(len(tracks), dtype=self.event_sel_dtype)
+        track_sel = np.zeros(len(tracks['id'].compressed()), dtype=self.track_sel_dtype)
         event_sel['sel'] = event_is_stopping
         track_sel['sel'] = is_stopping.compressed()
         if self.is_mc:
             event_true_sel = np.zeros(len(tracks), dtype=self.event_sel_dtype)
             track_true_sel = np.zeros(len(tracks['id'].compressed()), dtype=self.track_sel_dtype)
             event_true_sel['sel'] = event_is_true_stopping
-            track_true_sel['sel'] = is_true_stopping[~tracks['id'].mask].ravel()
+            track_true_sel['sel'] = (is_true_stopping[~tracks['id'].mask].ravel()
+                                     & is_muon[~tracks['id'].mask].ravel())
 
         # reserve data space
         event_sel_slice = self.data_manager.reserve_data(
