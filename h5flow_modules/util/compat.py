66ac49d781270be9d8c828012c422ae4de051e15
peter-madigan <pmadigan@berkeley.edu>
2021-09-20 10:45:34 -0700

    Fix compatibility tests and error message

--- h5flow_modules/util/compat.py
+++ h5flow_modules/util/compat.py
@@ -17,4 +17,4 @@ def assert_compat_version(version, other_version):
     other_version = other_version.split('.')
 
     assert version[0] == other_version[0], f'Major version incompatible! Requires {version[0]} == {other_version[0]}'
-    assert int(version[1]) <= int(other_version[1]), f'Minor version incompatible! Requires {version[0]}.{version[1]} >= {other_version[0]}.{other_version[1]}'
+    assert int(version[1]) <= int(other_version[1]), f'Minor version incompatible! Requires {version[0]}.{version[1]} <= {other_version[0]}.{other_version[1]}'
