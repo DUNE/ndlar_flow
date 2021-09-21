Fix dEdx calculation bugs, use gaussian approximation for dqdx profile

h5flow_modules/analysis/stopping_muon_selection.py
@@ -36,6 +36,7 @@ class StoppingMuonSelection(H5FlowStage):
     default_profile_dx = 20  # mm
     default_profile_max_range = 1600  # mm
     default_larpix_gain = 250  # e/mV
+    default_larpix_noise = 1000 # e
     default_hits_dset_name = 'charge/hits'
     default_merged_dset_name = 'combined/tracklets/merged'
     default_t0_dset_name = 'combined/t0'
@@ -73,6 +74,7 @@ class StoppingMuonSelection(H5FlowStage):
         self.cathode_fid_cut = params.get('cathode_fid_cut', self.default_cathode_fid_cut)
         self.length_cut = params.get('length_cut', self.default_length_cut)
         self.larpix_gain = params.get('larpix_gain', self.default_larpix_gain)
+        self.larpix_noise = params.get('larpix_noise', self.default_larpix_noise)
         self.profile_dx = params.get('profile_dx', self.default_profile_dx)
         self.profile_max_range = params.get('profile_max_range',
                                             self.default_profile_max_range)
@@ -123,15 +125,15 @@ class StoppingMuonSelection(H5FlowStage):
 
         # convert mean dE/dx entries to MPV dE/dx
         self.muon_range_table['dedx_mpv'] = resources['ParticleData'].landau_peak(
-            self.muon_range_table['t'], resources['ParticleData'].mu_mass)
+            self.muon_range_table['t'], resources['ParticleData'].mu_mass, self.profile_dx) / self.profile_dx
         self.proton_range_table['dedx_mpv'] = resources['ParticleData'].landau_peak(
-            self.proton_range_table['t'], resources['ParticleData'].p_mass)
-
+            self.proton_range_table['t'], resources['ParticleData'].p_mass, self.profile_dx) / self.profile_dx
+        
         # calculate recombination correction
         muon_r = resources['LArData'].ionization_recombination(
-            self.muon_range_table['dedx_mpv'])
+            self.muon_range_table['dedx'])
         proton_r = resources['LArData'].ionization_recombination(
-            self.proton_range_table['dedx_mpv'])
+            self.proton_range_table['dedx'])
         w = resources['LArData'].ionization_w
 
         self.muon_range_table['dqdx'] = (muon_r * self.muon_range_table['dedx_mpv']
@@ -140,10 +142,19 @@ class StoppingMuonSelection(H5FlowStage):
                                            / w)
         self.muon_range_table['dqdx_width'] = (
             muon_r / w * resources['ParticleData'].landau_width(self.muon_range_table['t'],
-                                                                resources['ParticleData'].mu_mass))
+                                                                resources['ParticleData'].mu_mass, self.profile_dx) / self.profile_dx)
         self.proton_range_table['dqdx_width'] = (
             proton_r / w * resources['ParticleData'].landau_width(self.proton_range_table['t'],
-                                                                  resources['ParticleData'].p_mass))
+                                                                  resources['ParticleData'].p_mass, self.profile_dx) / self.profile_dx)
+        noise = self.larpix_noise * np.sqrt(self.profile_dx / resources['Geometry'].pixel_pitch) / resources['Geometry'].pixel_pitch
+        self.muon_range_table['dqdx_width'] = np.sqrt(noise**2 + self.muon_range_table['dqdx_width']**2)
+        self.proton_range_table['dqdx_width'] = np.sqrt(noise**2 + self.proton_range_table['dqdx_width']**2)
+        
+        self.data_manager.set_attrs(self.path, proton_dqdx=self.proton_range_table['dqdx'], muon_dqdx=self.muon_range_table['dqdx'],
+                                    proton_dqdx_width=self.proton_range_table['dqdx_width'], muon_dqdx_width=self.muon_range_table['dqdx_width'],
+                                    proton_dedx=self.proton_range_table['dedx_mpv'], muon_dedx=self.muon_range_table['dedx_mpv'],
+                                    proton_range=self.proton_range_table['range'], muon_range=self.muon_range_table['range'])
+                                    
 
     def create_regions(self):
         x = resources['Geometry'].pixel_xy.compress((0,))
@@ -228,7 +239,8 @@ class StoppingMuonSelection(H5FlowStage):
         interp_dqdx = interp(np.clip(profile_rr, min_range, max_range))
         interp_dqdx_width = interp_width(np.clip(profile_rr, min_range, max_range))
 
-        return stats.moyal.pdf(profile_dqdx, loc=interp_dqdx, scale=interp_dqdx_width)
+#         return stats.moyal.pdf(profile_dqdx, loc=interp_dqdx, scale=interp_dqdx_width)
+        return stats.norm.pdf(profile_dqdx, loc=interp_dqdx, scale=interp_dqdx_width)
 
     @staticmethod
     def profiled_dqdx(tracks, seed_pt, hit_xyz, hit_q, dx, max_range, mask=None):
@@ -361,11 +373,11 @@ class StoppingMuonSelection(H5FlowStage):
 
         end_pt = np.take_along_axis(pos, np.argmax(dq, axis=-1)[..., np.newaxis, np.newaxis], axis=-2)
 
-        r_dq = np.empty((orig_len,) + dq.shape[1:])
-        r_dn = np.empty((orig_len,) + dn.shape[1:])
-        r_start_pt = np.empty((orig_len,) + start_pt.shape[1:])
-        r_end_pt = np.empty((orig_len,) + end_pt.shape[1:])
-        r_pos = np.empty((orig_len,) + pos.shape[1:])
+        r_dq = np.zeros((orig_len,) + dq.shape[1:])
+        r_dn = np.zeros((orig_len,) + dn.shape[1:])
+        r_start_pt = np.zeros((orig_len,) + start_pt.shape[1:])
+        r_end_pt = np.zeros((orig_len,) + end_pt.shape[1:])
+        r_pos = np.zeros((orig_len,) + pos.shape[1:])
 
         np.place(r_dq, np.broadcast_to(mask[..., np.newaxis], r_dq.shape), dq)
         np.place(r_dn, np.broadcast_to(mask[..., np.newaxis], r_dn.shape), dn)
@@ -467,7 +479,7 @@ class StoppingMuonSelection(H5FlowStage):
         profile_rr = ((np.expand_dims(np.argmax(profile_dqdx, axis=-1), axis=-1)
                        - np.indices(profile_dqdx.shape)[-1] + 0.5) * self.profile_dx)
         profile_rr = ma.masked_where(dn <= 0, profile_rr)
-
+        
         muon_likelihood = self.profile_likelihood(profile_rr.data, profile_dqdx.data,
                                                   self.muon_range_table)
         proton_likelihood = self.profile_likelihood(profile_rr.data, profile_dqdx.data,
@@ -476,9 +488,9 @@ class StoppingMuonSelection(H5FlowStage):
                                                  profile_dqdx.data,
                                                  self.muon_range_table)
         # mask invalid values
-        muon_likelihood[dn == 0] = -1
-        proton_likelihood[dn == 0] = -1
-        mip_likelihood[dn == 0] = -1
+        muon_likelihood[(dn == 0) | (profile_rr <= 0)] = -1
+        proton_likelihood[(dn == 0) | (profile_rr <= 0)] = -1
+        mip_likelihood[(dn == 0) | (profile_rr <= 0)] = -1
 
         muon_likelihood = ma.masked_values(muon_likelihood, -1)
         proton_likelihood = ma.masked_values(proton_likelihood, -1)

h5flow_modules/resources/particle_data.py
@@ -47,7 +47,7 @@ class ParticleData(H5FlowResource):
     default_muon_range_table_path = 'PDG_muon_range_table_Ar.txt'
     default_proton_range_table_path = 'NIST_proton_range_table_Ar.txt'
 
-    _K = 0.307075 * units.MeV / (units.cm)**2
+    _K = 0.307075 * units.MeV * (units.cm)**2
 
     #: electron mass
     e_mass = 510.9989461 * units.keV
@@ -85,15 +85,17 @@ class ParticleData(H5FlowResource):
             self.data = dict()
 
             # appropriate units from tables
-            self.data['muon_range'] = muon_table['range'] * units.cm
+            self.data['muon_range'] = (muon_table['range'] * units.g / (units.cm)**2
+                                       / resources['LArData'].density)
             self.data['muon_t'] = muon_table['t'] * units.MeV
-            self.data['muon_dedx'] = (muon_table['dedx'] * units.g
-                                      * units.MeV / units.cm
+            self.data['muon_dedx'] = (muon_table['dedx'] / units.g
+                                      * units.MeV * units.cm**2
                                       * resources['LArData'].density)
-            self.data['proton_range'] = proton_table['range'] * units.cm
+            self.data['proton_range'] = (proton_table['range'] * units.g / (units.cm)**2
+                                         / resources['LArData'].density)
             self.data['proton_t'] = proton_table['t'] * units.MeV
-            self.data['proton_dedx'] = (proton_table['dedx'] * units.g
-                                        * units.MeV / units.cm
+            self.data['proton_dedx'] = (proton_table['dedx'] / units.g
+                                        * units.MeV * units.cm**2
                                         * resources['LArData'].density)
 
             self.data['classname'] = self.classname
@@ -124,35 +126,39 @@ class ParticleData(H5FlowResource):
         return dict(range=self.data['proton_range'], t=self.data['proton_t'],
                     dedx=self.data['proton_dedx'])
 
-    def landau_width(self, t, mass):
+    def landau_width(self, t, mass, dx):
         ''' Moyal scale factor for Landau dE/dx width in LAr '''
         e = t + mass
         p = np.sqrt(e**2 - mass**2)
         beta = p / e
 
-        Z = resources['LArData'].Z
-        A = resources['LArData'].A
-        ksi = self._K / 2 * (Z / A) * resources['LArData'].density / beta**2
+        ksi = self._ksi(dx, beta)
 
-        return (4 * ksi / 3.59)
+        rv = (4 * ksi / 3.59)
+        return rv
 
-    def landau_peak(self, t, mass):
+    def landau_peak(self, t, mass, dx):
         ''' Moyal peak location for Landau dE/dx distribution in LAr '''
         e = t + mass
         p = np.sqrt(e**2 - mass**2)
         beta = p / e
         gamma = e / mass
 
-        Z = resources['LArData'].Z
-        A = resources['LArData'].A
-        ksi = self._K / 2 * (Z / A) * resources['LArData'].density / beta**2
+        ksi = self._ksi(dx, beta)
         I = 188.0 * units.eV
 
-        t0 = np.log(2 * self.e_mass * (beta * gamma)**2 / I)
+        t0 = np.log(2 * self.e_mass * beta**2 * gamma**2 / I)
         t1 = np.log(ksi / I)
-        t2 = 0.200 - beta**2 + self._delta(beta * gamma)
+        t2 = 0.200 - beta**2 - self._delta(beta * gamma)
 
-        return ksi * (t0 + t1 + t2)
+        rv = ksi * (t0 + t1 + t2)
+        return rv
+    
+    def _ksi(self, x, beta):
+        Z = resources['LArData'].Z
+        A = resources['LArData'].A
+        ksi = (self._K / 2) * (Z / A) * (resources['LArData'].density * x) / (beta**2)
+        return ksi
 
     @staticmethod
     def _delta(betagamma):
@@ -162,11 +168,12 @@ class ParticleData(H5FlowResource):
         x1 = 3.0
         cbar = 5.2146
         k = 3.00
+        x = np.log10(betagamma)
 
-        return (betagamma < x0) * (
-            (betagamma < x1) * (2 * np.log(10) * betagamma - cbar + a * (x1 - betagamma)**k)
-            + (betagamma > x1) * (2 * np.log(10) * betagamma - cbar))
-
+        return (x < x0) * (
+            (x < x1) * (2 * np.log(10) * x - cbar + a * (x1 - x)**k)
+            + (x > x1) * (2 * np.log(10) * x - cbar))
+        
     @staticmethod
     def load_nist_range_table(path):
         '''