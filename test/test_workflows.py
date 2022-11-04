a6efcacc430b7d42203736c194f4ca6d5c7e7576
peter-madigan <pmadigan@berkeley.edu>
2022-11-03 22:42:12 -0700

    Update tests

--- test/test_workflows.py
+++ test/test_workflows.py
@@ -57,14 +57,12 @@ def light_event_built_file(pytestconfig, tmp_path_factory, light_source_file, ru
                            electron_lifetime_file, tmp_h5_file):
     filename = next(maybe_fetch_from_url(pytestconfig, tmp_path_factory,
                                      ('https://portal.nersc.gov/project/dune/'
-                                      'data/Module0/merged/prod2/light_event/'
-                                      'events_2021_04_04_00_41_40_CEST.gz.h5')))
-    with h5py.File(filename, 'r') as fi:
-        with h5py.File(tmp_h5_file,'a') as fo:
-            fo.copy(fi['light'],'light')
-    '''
+                                      'data/Module0/LRS/0a7b54bd_20210404_004206.data')))
+    next(maybe_fetch_from_url(pytestconfig, tmp_path_factory,
+                              ('https://portal.nersc.gov/project/dune/'
+                               'data/Module0/LRS/0a7a314c_20210404_004206.data')))
     print('Light event building...')
-    h5flow.run(['h5flow_yamls/workflows/light/light_event_building.yaml'],
+    h5flow.run(['h5flow_yamls/workflows/light/light_event_building_adc64.yaml'],
                tmp_h5_file,
                light_source_file,
                verbose=2,
@@ -75,7 +73,6 @@ def light_event_built_file(pytestconfig, tmp_path_factory, light_source_file, ru
         'light/events/data',
         'light/wvfm/data',
     ))
-    '''
     return tmp_h5_file
 
 
