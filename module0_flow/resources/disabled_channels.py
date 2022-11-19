06cb53e96b9a703c71abeaf9bd2ccf72277b0093
peter-madigan <pmadigan@berkeley.edu>
2022-11-19 12:34:54 -0800

    Allow disabled channels to be network agnostic

--- module0_flow/resources/disabled_channels.py
+++ module0_flow/resources/disabled_channels.py
@@ -167,6 +167,15 @@ class DisabledChannels(H5FlowResource):
                     chip_id.append(int(chip_id_))
                     channel_id.append(int(ch))
 
+                    if resources['Geometry'].network_agnostic == True:
+                        # add additional entries for each io channel
+                        start_io_channel = ((io_channel_-1)//self.n_io_channels_per_tile)*self.n_io_channels_per_tile + 1
+                        for io_channel in range(start_io_channel, start_io_channel+self.n_io_channels_per_tile):
+                            io_group.append(int(io_group_))
+                            io_channel.append(int(io_channel)))
+                            chip_id.append(int(chip_id_))
+                            channel_id.append(int(ch))
+
             pixel_xy = resources['Geometry'].pixel_xy
             chip_key = (np.array(io_group), np.array(io_channel),
                         np.array(chip_id), np.array(channel_id))
