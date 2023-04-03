10e760a345ff4c38da9efdffcc938d15ae8d012e
Kevin Wood <kwood@lbl.gov>
2023-04-03 10:34:26 -0700

    updates example proto_nd_flow run script

--- scripts/proto_nd_scripts/run_proto_nd_flow_example.sh
+++ scripts/proto_nd_scripts/run_proto_nd_flow_example.sh
@@ -5,12 +5,14 @@
 # to download all the necessary inputs into the correct directories
 #
 
-#INPUT_FILE=$1
-INPUT_FILE='data/proto_nd_flow/sim2x2_challenge_5xNuMIspills_ME_65E12POTperSpill_larndsim_mockdata_v1.h5'
+INPUT_FILE=$1
 
-# below is the same file with mc information included
-#INPUT_FILE='/home/kwood/research/dune/2x2/data/simulation_challenge_26Oct2022/sim2x2_challenge_5xNuMIspills_ME_65E12POTperSpill_v2.larndsim.h5'
-OUTPUT_FILE=${INPUT_FILE//.h5/.proto_nd_flow.h5}
+OUTPUT_DIR=`pwd` #!!! change me
+OUTPUT_NAME=(${INPUT_FILE//"/"/ })
+OUTPUT_NAME=${OUTPUT_NAME[-1]}
+OUTPUT_FILE="${OUTPUT_DIR}/${OUTPUT_NAME}"
+OUTPUT_FILE=${OUTPUT_FILE//.h5/.proto_nd_flow.h5}
+echo ${OUTPUT_FILE}
 
 # for running on a login node
 H5FLOW_CMD='h5flow'
@@ -20,21 +22,19 @@ H5FLOW_CMD='h5flow'
 # run all stages
 WORKFLOW1='yamls/proto_nd_flow/workflows/charge/charge_event_building.yaml'
 WORKFLOW2='yamls/proto_nd_flow/workflows/charge/charge_event_reconstruction.yaml'
-#WORKFLOW3='h5flow_yamls/workflows/combined/combined_reconstruction.yaml'
-
-# assumes this is being run from scripts/proto_nd_scripts
+WORKFLOW3='yamls/proto_nd_flow/workflows/combined/combined_reconstruction.yaml'
+WORKFLOW4='yamls/proto_nd_flow/workflows/charge/prompt_calibration.yaml'
 
 HERE=`pwd`
-cd ../../
+cd ndlar_flow
 
-
+# avoid being asked if we want to overwrite the file if it exists.
+# this is us answering "yes".
 if [ -e $OUTPUT_FILE ]; then
     rm -i $OUTPUT_FILE
 fi
 
-#$H5FLOW_CMD -c $WORKFLOW1 $WORKFLOW2 $WORKFLOW3 -i $INPUT_FILE -o $OUTPUT_FILE
-$H5FLOW_CMD -c $WORKFLOW1 $WORKFLOW2 -i $INPUT_FILE -o $OUTPUT_FILE
-#$H5FLOW_CMD -c $WORKFLOW1 -i $INPUT_FILE -o $OUTPUT_FILE
+$H5FLOW_CMD -c $WORKFLOW1 $WORKFLOW2 $WORKFLOW3 $WORKFLOW4 -i $INPUT_FILE -o $OUTPUT_FILE
 
 echo "Done!"
 echo "Output can be found at $OUTPUT_FILE"
