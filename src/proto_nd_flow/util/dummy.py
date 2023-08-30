1cdddcd7c93d402022f8a790bff4c35614489a03
Kathryn Sutton <ksutton@login19.chn>
2023-08-30 14:17:06 -0700

    committing before reverting dummy script

--- src/proto_nd_flow/util/dummy.py
+++ src/proto_nd_flow/util/dummy.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 
 from h5flow.core import H5FlowStage, resources
 
-from proto_nd_flow.reco.charge.calib_final_hits import CalibHitBuilder
+from proto_nd_flow.reco.charge.calib_prompt_hits import CalibHitBuilder
 
 
 class Dummy(H5FlowStage):
@@ -15,30 +15,32 @@ class Dummy(H5FlowStage):
     class_version = '0.0.0'
     defaults = dict(
         events_dset_name = 'charge/events',
-        hits_name = 'charge/calib_final_hits',
-        hit_charge_name = 'charge/calib_final_hits',
+        hits_name = 'charge/calib_prompt_hits',
+        hit_charge_name = 'charge/calib_prompt_hits',
         hits_hough_name = 'charge/calib_hough_hits'
         )
     hough_dtype = CalibHitBuilder.calib_hits_dtype
 
+    '''
     def __init__(self, **params):
         print('running dummy.py!!!!!')
         super(Dummy, self).__init__(**params)
         for key in self.defaults:
             setattr(self, key, params.get(key, self.defaults[key]))
-        self.merge_mode = self.merge_mode.lower()
-        assert self.merge_mode in self.valid_merge_modes, f'invalid merge mode: {self.merge_mode}'
+   #     self.merge_mode = self.merge_mode.lower()
+   #     assert self.merge_mode in self.valid_merge_modes, f'invalid merge mode: {self.merge_mode}'
 
     def init(self, source_name):
         print('running dummy.py!!!!!')
         super(Dummy, self).init(source_name)
 
-        self.hit_frac_dtype = np.dtype([
-            ('fraction', f'({self.max_contrib_segments},)f8'),
-            ('segment_id', f'({self.max_contrib_segments},)u8')
-        ])
+    #    self.hit_frac_dtype = np.dtype([
+     #       ('fraction', f'({self.max_contrib_segments},)f8'),
+    #        ('segment_id', f'({self.max_contrib_segments},)u8')
+    #    ])
 
         self.data_manager.create_dset(self.hits_hough_name, dtype=self.hough_dtype)
         self.data_manager.create_ref(self.hits_name, self.hits_hough_name)
         self.data_manager.create_ref(source_name, self.hits_hough_name)
         self.data_manager.create_ref(self.events_dset_name, self.hits_hough_name)
+        '''
