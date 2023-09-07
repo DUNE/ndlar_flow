febd0a103e22942577ba6f227bcfdc035f6153b4
Kathryn Sutton <ksutton@login19.chn>
2023-09-06 20:53:33 -0700

    commented out a bit more of the backtracking

--- src/proto_nd_flow/util/hough.py
+++ src/proto_nd_flow/util/hough.py
@@ -14,6 +14,7 @@ class hough(H5FlowStage):
     The goal is to take the charge/calib_prompt_hits and perform a Hough transform
     This could also be performed on calib_merged_hits that are in the "final" stage
     The outputs are saved as a set of hits along the line that are a subset of the inital input hits
+    Currently no hough transform implemented!!! Just an empty module
     ksutton 8/30/23
       '''
     class_version = '0.0.0'
@@ -21,44 +22,36 @@ class hough(H5FlowStage):
         events_dset_name = 'charge/events',
         hits_name = 'charge/calib_prompt_hits',
         hit_charge_name = 'charge/calib_prompt_hits',
-        merged_name = 'charge/hits/calib_merged_hits',
-        max_merge_steps = 5,
-        max_contrib_segments = 200,
-        merge_mode = 'last-first',
-        merge_cut = 50, # CRS ticks
-        mc_hit_frac_dset_name = 'mc_truth/calib_final_hit_backtrack'
+        output_name = 'charge/hits/calib_hough_hits',
+        #mc_hit_frac_dset_name = 'mc_truth/calib_hough_hit_backtrack'
         )
-    valid_merge_modes = ['last-first', 'pairwise']
 
-    merged_dtype = CalibHitBuilder.calib_hits_dtype
-
-    sum_fields = ['Q','E']
-    weighted_mean_fields = ['t_drift', 'ts_pps','x']
+    output_dtype = CalibHitBuilder.calib_hits_dtype
 
     def __init__(self, **params):
         super(hough, self).__init__(**params)
         for key in self.defaults:
             setattr(self, key, params.get(key, self.defaults[key]))
-        self.merge_mode = self.merge_mode.lower()
-        assert self.merge_mode in self.valid_merge_modes, f'invalid merge mode: {self.merge_mode}'
+     #   self.output_mode = self.output_mode.lower()
+     #   assert self.output_mode in self.valid_output_modes, f'invalid output mode: {self.output_mode}'
 
     def init(self, source_name):
         super(hough, self).init(source_name)
 
-        self.hit_frac_dtype = np.dtype([
-            ('fraction', f'({self.max_contrib_segments},)f8'),
-            ('segment_id', f'({self.max_contrib_segments},)u8')
-        ])
+       # self.hit_frac_dtype = np.dtype([
+       #     ('fraction', f'({self.max_contrib_segments},)f8'),
+       #     ('segment_id', f'({self.max_contrib_segments},)u8')
+       # ])
 
-        self.data_manager.create_dset(self.merged_name, dtype=self.merged_dtype)
-        self.data_manager.create_dset(self.mc_hit_frac_dset_name, dtype=self.hit_frac_dtype)
-        self.data_manager.create_ref(self.hits_name, self.merged_name)
-        self.data_manager.create_ref(source_name, self.merged_name)
-        self.data_manager.create_ref(self.merged_name,self.mc_hit_frac_dset_name)
-        self.data_manager.create_ref(self.events_dset_name, self.merged_name)
+        self.data_manager.create_dset(self.output_name, dtype=self.output_dtype)
+       # self.data_manager.create_dset(self.mc_hit_frac_dset_name, dtype=self.hit_frac_dtype)
+        self.data_manager.create_ref(self.hits_name, self.output_name)
+        self.data_manager.create_ref(source_name, self.output_name)
+       # self.data_manager.create_ref(self.output_name,self.mc_hit_frac_dset_name)
+        self.data_manager.create_ref(self.events_dset_name, self.output_name)
 
     #@staticmethod
-    def merge_hits(self,hits, weights, seg_fracs, dt_cut, sum_fields=None, weighted_mean_fields=None, max_steps=-1, mode='last-first'):
+    def output_hits(self,hits, weights, seg_fracs):
         '''
         currently does nothing, need to add in Hough transform here
 '''
@@ -73,12 +66,12 @@ class hough(H5FlowStage):
         old_id_mask = hits.mask['id'].copy()[...,np.newaxis]
         
         new_hit_idx = np.broadcast_to(np.cumsum(~mask.ravel(), axis=0).reshape(mask.shape + (1,)), old_ids.shape)-1
-        back_track = np.full(shape=new_hits.shape,fill_value=0.,dtype=self.hit_frac_dtype)
+  #      back_track = np.full(shape=new_hits.shape,fill_value=0.,dtype=self.hit_frac_dtype)
        
         return (
             ma.array(new_hits, mask=mask),
             np.c_[np.extract(~(old_id_mask | mask[...,np.newaxis]), old_ids), np.extract(~(old_id_mask | mask[...,np.newaxis]), new_hit_idx)],
-            ma.array(back_track, mask=mask)
+   #         ma.array(back_track, mask=mask)
             )
 
     def run(self, source_name, source_slice, cache):
@@ -91,21 +84,23 @@ class hough(H5FlowStage):
         hits = cache[self.hits_name]
 
         #get the new hits, references, and backtracking for the 
-        merged, ref, back_track = self.merge_hits(hits, weights=hits['Q'], seg_fracs=[packet_seg_bt,packet_frac_bt],dt_cut=self.merge_cut, sum_fields=self.sum_fields, weighted_mean_fields=self.weighted_mean_fields, max_steps=self.max_merge_steps, mode=self.merge_mode)
+  #      output, ref, back_track = self.output_hits(hits, weights=hits['Q'], seg_fracs=[packet_seg_bt,packet_frac_bt])
+        output, ref = self.output_hits(hits, weights=hits['Q'], seg_fracs=[packet_seg_bt,packet_frac_bt])
 
-        merged_mask = merged.mask['id'] #not sure what this does yet
+
+        output_mask = output.mask['id'] #not sure what this does yet
 
         # first write the new hits to the file 
-        new_nhit = int((~merged_mask).sum())
-        merge_slice = self.data_manager.reserve_data(self.merged_name, new_nhit)
-        merge_idx = np.r_[merge_slice].astype(merged.dtype['id'])
+        new_nhit = int((~output_mask).sum())
+        output_slice = self.data_manager.reserve_data(self.output_name, new_nhit)
+        output_idx = np.r_[output_slice].astype(output.dtype['id'])
         if new_nhit > 0:
-            ref[:,1] += merge_idx[0] # offset references based on reserved region in output file
-            np.place(merged['id'], ~merged_mask, merge_idx)
+            ref[:,1] += output_idx[0] # offset references based on reserved region in output file
+            np.place(output['id'], ~output_mask, output_idx)
 
-        self.data_manager.write_data(self.merged_name, merge_idx, merged[~merged_mask])
-        merge_bt_slice = self.data_manager.reserve_data(self.mc_hit_frac_dset_name, new_nhit)
-        self.data_manager.write_data(self.mc_hit_frac_dset_name, merge_idx, back_track[~merged_mask])
+        self.data_manager.write_data(self.output_name, output_idx, output[~output_mask])
+        #output_bt_slice = self.data_manager.reserve_data(self.mc_hit_frac_dset_name, new_nhit)
+        #self.data_manager.write_data(self.mc_hit_frac_dset_name, output_idx, back_track[~output_mask])
 
         # HACK: Remove duplicate refs. Would be nice to actually understand and
         # fix the origin of these duplicates.
@@ -114,172 +109,9 @@ class hough(H5FlowStage):
         ref = ref[np.argsort(ref[:, 0])]
 
         # finally, write the references
-        self.data_manager.write_ref(self.hits_name, self.merged_name, ref)
-        self.data_manager.write_ref(self.merged_name,self.mc_hit_frac_dset_name,np.c_[merge_idx,merge_idx])
-        ev_ref = np.c_[(np.indices(merged_mask.shape)[0] + source_slice.start)[~merged_mask], merge_idx]
-        self.data_manager.write_ref(source_name, self.merged_name, ev_ref)
-        self.data_manager.write_ref(self.events_dset_name, self.merged_name, ev_ref)
-
-'''
-        hit_contributions = np.full(shape=weights.shape+(3,self.max_contrib_segments),fill_value=0.)
-        #print('weights shape',weights.shape) 
-        #print('hit_contr shape',hit_contributions.shape) 
-        for it, q in np.ndenumerate(weights):
-            #print('it',it)
-            #hit_contributions[it].append([])
-            #print('---------------')
-            if len(new_frac_bt[it]) > 1:
-                print('!!!!!!!!!!!!!!!!!')
-                break
-            counter=0
-            for entry_it, entry in enumerate(new_frac_bt[it][0]):
-                #print('frac =',entry)
-                if abs(entry) < 0.001: continue
-                hit_contributions[it][0][counter] = q
-                hit_contributions[it][1][counter] = new_frac_bt[it][0][entry_it]
-                hit_contributions[it][2][counter] = new_seg_bt[it][0][entry_it]['segment_id']
-                counter+=1
-
-        #print('hit_contributions.shape =',hit_contributions.shape)
-        #print(hit_contributions)
-
-        while new_hits.size > 0 and iteration_count != max_steps:
-            iteration_count += 1
-            # algorithm is iterative, but typically only needs to loop a few (~2-3) times
-            # so we'll spit a warning if we reach the maximum number of steps
-            if iteration_count == max_steps:
-                logging.info(f'Hit merging algorithm reached max step limit {max_steps}')
-
-            # sort array along last axis to find groups of hits on the same channel, use a stable sort with the aim of improving performance on later iterations
-            isort = np.argsort(ma.array(new_hits, mask=mask), axis=-1, order=['z','y','ts_pps','t_drift'], kind='stable')
-            mask = np.take_along_axis(mask, isort, axis=-1)
-            new_hits = np.take_along_axis(new_hits, isort, axis=-1)
-            weights = np.take_along_axis(weights, isort, axis=-1)
-            hit_contributions = np.take_along_axis(hit_contributions,isort[...,np.newaxis,np.newaxis],axis=-3)
-            old_ids = np.take_along_axis(old_ids, isort[...,np.newaxis], axis=-2)
-            old_id_mask = np.take_along_axis(old_id_mask, isort[...,np.newaxis], axis=-2)
-            N_new_hits = new_hits.shape[0]*new_hits.shape[1]-np.count_nonzero(mask)
-            print('current number of merged hits =',N_new_hits)
-            
-            # identify neighboring hits on the same channel
-            dt = np.abs(np.diff(new_hits['ts_pps'].astype(int), axis=-1))
-            same_channel = (
-                (new_hits['z'][..., :-1] == new_hits['z'][..., 1:])
-                & (new_hits['y'][..., :-1] == new_hits['y'][..., 1:])
-            )
-
-            # flag valid hits if they are on the same channel and are close in time
-            to_merge = (dt < dt_cut) & same_channel & ~mask[...,:-1] & ~mask[...,1:]
-
-            if mode == 'last-first':
-                # only combine unambiguous pairs of hits on a channel on each iteration
-                to_merge[...,:-1] = ~to_merge[...,1:] & to_merge[...,:-1]
-            elif mode == 'pairwise':
-                # combine every available pair of hits on each iteration
-                to_merge[...,:-1] = to_merge[...,:-1] & (np.cumsum(to_merge, axis=-1) % 2 == 0)[...,:-1]
-            else:
-                raise RuntimeError(f'invalid merge mode: {mode}')
-
-            print("merging:",np.count_nonzero(to_merge))
-
-            # exits loop if no remaining hits to combine
-            if np.any(to_merge):
-                # move 2nd hit into position of first hit, combining attributes along the way
-                hit0 = np.extract(to_merge, new_hits[...,:-1])
-                hit1 = np.extract(to_merge, new_hits[...,1:])
-
-                # these fields will be summed hit[i][field] -> hit[i+1][field] + hit[i][field]
-                for field in sum_fields:
-                    if field in new_hits.dtype.names:
-                        np.place(new_hits[...,:-1][field], to_merge, hit0[field] + hit1[field])
-
-                # these fields will use the charge-weighted average hit[i][field] -> (hit[i+1][field] * q[i+1] + hit[i][field] * q[i]) / (q[i+1] + q[i])
-                q0 = np.extract(to_merge, weights[...,:-1])
-                q1 = np.extract(to_merge, weights[...,1:])
-                qsum = np.abs(q0) + np.abs(q1)
-                # regularize so there are no nans
-                qsum = np.where(qsum == 0, 1e-300, qsum)
-                # it is not obvious how to treat the possibility of negative charge values (e.g. noise)
-                # this should(?) be rare, so we'll just spit out a warning
-                if np.any((q0 < 0) | (q1 < 0)):
-                    logging.info(f'Hit merging encountered negative value(s) (count={((q0 < 0) | (q1 < 0)).sum()}) in charge weighting, results may be unreliable')
-                w0 = np.abs(q0)/qsum
-                w1 = np.abs(q1)/qsum
-                for field in weighted_mean_fields:
-                    if field in new_hits.dtype.names:
-                        base = np.minimum(hit0[field], hit1[field]) # improves precision of weighted sum if values are large (e.g. timestamps)
-                        np.place(new_hits[...,:-1][field], to_merge, ((hit0[field]-base) * w0 + (hit1[field]-base) * w1).astype(new_hits.dtype[field]) + base)
-                # combine weights for next iteration
-                np.place(weights[...,:-1], to_merge, weights[...,:-1] + weights[...,1:])
-                for hit_it, hit_cont in np.ndenumerate(weights[...,:-1]):
-                    if (not to_merge[hit_it]) | mask[hit_it]:
-                        #print('skipping')
-                        continue
-                    #if hit_contributions[hit_it][1].shape[0] < self.max_contrib_segments: print('a shape :',hit_contributions[hit_it][1].shape)
-                    e = np.argwhere(hit_contributions[...,:-1][hit_it][1]==0)[0][0]
-                    f = np.argwhere(hit_contributions[...,:][hit_it[0],hit_it[1]+1][1]==0)[0][0]
-                    # merge the hit contributions:
-                    for comb_it in range(f):
-                        hit_contributions[...,:-1][hit_it][1][e+comb_it] = hit_contributions[...,:][hit_it[0],hit_it[1]+1][1][comb_it]
-                        hit_contributions[...,:-1][hit_it][0][e+comb_it] = hit_contributions[...,:][hit_it[0],hit_it[1]+1][0][comb_it]
-                        hit_contributions[...,:-1][hit_it][2][e+comb_it] = hit_contributions[...,:][hit_it[0],hit_it[1]+1][2][comb_it]
-                        # and remove them from the hit that was merged in
-                        hit_contributions[hit_it[0],hit_it[1]+1][1][comb_it] = 0
-                        hit_contributions[hit_it[0],hit_it[1]+1][0][comb_it] = 0.
-                        hit_contributions[hit_it[0],hit_it[1]+1][2][comb_it] = 0.
-
-                # now we mask off hits that have already been merged
-                mask[...,1:] = mask[...,1:] | to_merge
-
-                # and track the hit ids of the hits that were merged by propogating the indices forward
-                if mode == 'last-first':
-                    old_id_mask = np.concatenate([old_id_mask[...,0:1], old_id_mask], axis=-1)
-                    old_ids = np.concatenate([old_ids[...,0:1], old_ids], axis=-1)
-                    id_merge = np.broadcast_to(to_merge[...,np.newaxis], to_merge.shape + old_ids.shape[-1:])
-                    divider = 1
-                elif mode == 'pairwise':
-                    old_id_mask = np.concatenate([old_id_mask, old_id_mask], axis=-1)
-                    old_ids = np.concatenate([old_ids, old_ids], axis=-1)
-                    id_merge = np.broadcast_to(to_merge[...,np.newaxis], to_merge.shape + old_ids.shape[-1:])
-                    divider = old_ids.shape[-1]//2
-                else:
-                    raise RuntimeError(f'invalid mode {mode}')
-                # move ids from hit[i+1] to hit[i] (while keeping the ids for hit[i])
-                np.place(old_ids[...,:-1,divider:], id_merge[...,divider:], np.extract(id_merge[...,divider:], old_ids[...,1:,divider:]))
-                # copy the id mask for hit[i+1] into hit[i] (while keeping the id mask for hit[i])
-                np.place(old_id_mask[...,:-1,divider:], id_merge[...,divider:], np.extract(id_merge[...,divider:], old_id_mask[...,1:,divider:]))
-                # and clear the id mask for hit[i+1]
-                np.place(old_id_mask[...,1:,:], id_merge, True)
-            else:
-                break
-
-        # calculate segment contributions for each merged hit
-        tmp_bt = np.full(shape=new_hits.shape+(2,self.max_contrib_segments),fill_value=0.)
-       # loop over hits
-        for hit_it, hit in np.ndenumerate(new_hits):
-            if mask[hit_it]: continue
-            hit_contr = hit_contributions[hit_it]
-            # renormalize the fractional contributions given the charge weighted average
-            norm = np.sum(np.multiply(hit_contr[0],hit_contr[1]))
-            if norm == 0.: norm = 1.
-            tmp_bt[hit_it][0] = np.multiply(hit_contr[0],hit_contr[1])/norm # fractional contributions
-            tmp_bt[hit_it][1] = hit_contr[2] # segment_ids
-
-            # merge unique track contributions
-            track_dict = defaultdict(lambda:0)
-            for track in zip(tmp_bt[hit_it][0],tmp_bt[hit_it][1]):
-                track_dict[track[1]] += track[0]
-            track_dict = dict(track_dict)
-            bt_unique_segs = np.array(list(track_dict.keys()))
-            bt_unique_frac = np.array(list(track_dict.values()))
-            n_conts = bt_unique_frac.shape[0]
-            isort = np.flip(np.argsort(np.abs(bt_unique_frac), axis=-1, kind='stable'))
-            bt_unique_segs = np.take_along_axis(bt_unique_segs, isort, axis=-1)
-            bt_unique_frac = np.take_along_axis(bt_unique_frac, isort, axis=-1)
-            back_track[hit_it]['fraction'] = [0.]*self.max_contrib_segments
-            back_track[hit_it]['segment_id'] = [0]*self.max_contrib_segments
-            back_track[hit_it]['fraction'][:bt_unique_frac.shape[0]] = bt_unique_frac
-            back_track[hit_it]['segment_id'][:bt_unique_segs.shape[0]] = bt_unique_segs
-
-       '''
+        self.data_manager.write_ref(self.hits_name, self.output_name, ref)
+        #self.data_manager.write_ref(self.output_name,self.mc_hit_frac_dset_name,np.c_[output_idx,output_idx])
+        ev_ref = np.c_[(np.indices(output_mask.shape)[0] + source_slice.start)[~output_mask], output_idx]
+        self.data_manager.write_ref(source_name, self.output_name, ev_ref)
+        self.data_manager.write_ref(self.events_dset_name, self.output_name, ev_ref)
 
