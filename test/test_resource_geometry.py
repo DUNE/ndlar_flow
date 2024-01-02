3a893fefff0632bb00d237d13cd1ea0e8e5fae1d
Kevin Wood <kwood@lbl.gov>
2024-01-02 15:37:29 -0800

    change test from module 0 to 2x2

--- test/test_resource_geometry.py
+++ test/test_resource_geometry.py
@@ -1,7 +1,7 @@
 import pytest
 
 from h5flow.data import H5FlowDataManager
-from module0_flow.resources.geometry import Geometry
+from proto_nd_flow.resources.geometry import Geometry
 
 
 @pytest.fixture
