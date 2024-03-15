992b5a43695d1944a411e803bb69ac268727b07c
liviocali <liviocalivers@hotmail.com>
2024-03-15 14:52:58 +0100

    Update event builder to support batch_size>1

--- src/proto_nd_flow/reco/light/adc64_event_generator.py
+++ src/proto_nd_flow/reco/light/adc64_event_generator.py
@@ -82,11 +82,11 @@ class LightADC64EventGenerator(H5FlowGenerator):
         ('id', 'u8'),  # unique identifier
         ('event', 'i4'),  # event number in data file
         ('sn', 'i4', (self.n_adcs,)),  # adc serial number
-        ('ch', 'u1', (self.n_adcs, self.n_channels)),  # channel number
-        ('utime_ms', 'u8', (self.n_adcs, self.n_channels)),  # unix time [ms since epoch]
-        ('tai_ns', 'u8', (self.n_adcs, self.n_channels)),  # time since PPS [ns]
-        ('wvfm_valid', 'u1', (self.n_adcs, self.n_channels))  # boolean, 1 if channel present in event
-    ])
+        #('ch', 'u1', (self.n_adcs, self.n_channels)),  # channel number
+        ('utime_ms', 'u8', (self.n_adcs,)),  # unix time [ms since epoch]
+        ('tai_ns', 'u8', (self.n_adcs,)),  # time since PPS [ns]
+        ('wvfm_valid', 'u1', (self.n_adcs, self.n_channels))  # boolean, 1 if channel present in event    
+        ])
 
     def wvfm_dtype(self): return np.dtype([
         ('samples', 'i2', (self.n_adcs, self.n_channels, self.n_samples))  # sample value
@@ -105,23 +105,24 @@ class LightADC64EventGenerator(H5FlowGenerator):
 
         self.sn_table = params['sn_table']
 
-        self.input_file = adc64format.ADC64Reader(self.input_filename,self.n_adcs)
+        self.input_file = adc64format.MPDReader(self.input_filename,self.n_adcs)
         self.input_file.open()
         # Read run info
-        _, self.nbytes_runinfo, self.runinfo =adc64format.parse_run_start(self.input_file.stream)
+        _, self.nbytes_runinfo, self.runinfo =adc64format.mpd_parse_run_start(self.input_file.stream)
         # use the first file for the event reference
-        _, self.chunk_size, test_event = adc64format.parse_chunk(self.input_file.stream)
+        _, self.chunk_size, test_event = adc64format.mpd_parse_chunk(self.input_file.stream)
         ndevices = len(test_event['data'])
         total_length_b = self.input_file.stream.seek(0, 2)-self.nbytes_runinfo
         self.input_file.reset()
         self.n_samples = test_event['data'][0].dtype['voltage'].shape[-1]
 
+        #Positions in #chunks/events (not bytes)
         self.end_position = total_length_b // self.chunk_size if self.end_position is None else min(self.end_position, total_length_b // self.chunk_size)
-        self.start_position = self.nbytes_runinfo if self.start_position is None else self.start_position
+        self.start_position = 0 if self.start_position is None else self.start_position
         self.curr_position = self.start_position
 
         # skip to start position
-        self.input_file.stream.seek(self.start_position, 0)
+        self.input_file.stream.seek(self.nbytes_runinfo, 0)
 
     def __len__(self):
         return (self.end_position - self.start_position) // (self.batch_size)
@@ -166,9 +167,9 @@ class LightADC64EventGenerator(H5FlowGenerator):
         matched_events = None
         if self.rank == 0:
             # only read from single process
-            matched_events = [self.input_file.next(self.batch_size)]  #FIXME: only works with batch_size=1 at the moment (to be fixed in adc64format)
+            matched_events = self.input_file.next(self.batch_size)  #FIXME: only works with batch_size=1 at the moment (to be fixed in adc64format)
         # format events into output shape / structure
-        if matched_events[0] is not None:
+        if matched_events is not None:
             # create new event array / waveform array
             nevents = len(matched_events)
             event_arr = np.zeros(nevents, dtype=self.event_dtype)
@@ -178,22 +179,32 @@ class LightADC64EventGenerator(H5FlowGenerator):
                 data = np.array(events['data'])
                 device = np.array(events['device'])
                 time = np.array(events['time'])
-                event_arr[ievent]['event'] = event['event']
-                for iadc, sn in enumerate(self.sn_table):
-                    data_index = np.where(device["serial"] == sn)[0]
-                    channels = data[data_index]['channel']
-                    event_arr[ievent]['sn'][iadc] = device[data_index]['serial']
-                    event_arr[ievent]['utime_ms'][iadc] = 0
-                    event_arr[ievent]['tai_ns'][iadc] = time[data_index]['tai_s']*1e9 + time[data_index]['tai_ns']
-                    event_arr[ievent]['wvfm_valid'][iadc, channels] = True
-                    wvfm_arr[ievent]['samples'][iadc, channels] = data[data_index]['voltage']
+                try:
+                    event_arr[ievent]['event'] = event['event']
+                    for iadc, sn in enumerate(self.sn_table):
+                        data_index = np.where(device["serial"] == sn)[0]
+                        channels = data[data_index]['channel']
+                        event_arr[ievent]['sn'][iadc] = device[data_index]['serial']
+                        event_arr[ievent]['utime_ms'][iadc] = 0
+                        event_arr[ievent]['tai_ns'][iadc] = time[data_index]['tai_s']*1e9 + time[data_index]['tai_ns']
+                        event_arr[ievent]['wvfm_valid'][iadc, channels] = True
+                        wvfm_arr[ievent]['samples'][iadc, channels] = data[data_index]['voltage']
+                except:
+                    continue
 
             # apply different clock frequency
             event_arr['tai_ns'] = (event_arr['tai_ns'] * self.clock_timestamp_factor).astype(event_arr.dtype['tai_ns'].base)
+
             # mask off any totally empty events
             mask = np.any(event_arr['wvfm_valid'], axis=(-1,-2))
             event_arr = event_arr[mask]
             wvfm_arr = wvfm_arr[mask]
+            
+            # mask off any extraneous events
+            if self.curr_position + len(event_arr) > self.end_position:
+                mask = self.curr_position + np.arange(len(event_arr)) < self.end_position
+                event_arr = event_arr[mask]
+                wvfm_arr = wvfm_arr[mask]
             self.curr_position += len(event_arr)
         else:
             event_arr = np.empty((0,), dtype=self.event_dtype)
