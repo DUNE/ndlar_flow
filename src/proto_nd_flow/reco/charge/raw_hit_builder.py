082184ff6cf2959e6691a60ec4c2673b44ed4ab3
sam-fogarty <samuel.fogarty@gmail.com>
2024-04-21 12:54:27 -0700

    Add methods fro extracting pedestals and calculating larpix config parameters on the fly

--- src/proto_nd_flow/reco/charge/raw_hit_builder.py
+++ src/proto_nd_flow/reco/charge/raw_hit_builder.py
@@ -2,6 +2,7 @@ import numpy as np
 import numpy.lib.recfunctions as rfn
 from collections import defaultdict
 import json
+import h5py
 
 from h5flow.core import H5FlowStage, resources
 
@@ -98,11 +99,13 @@ class RawHitBuilder(H5FlowStage):
         self.ts_dset_name = params.get('ts_dset_name')
         self.pedestal_file = params.get('pedestal_file', '')
         self.configuration_file = params.get('configuration_file', '')
+        self.save_ped_json = params.get('save_ped_json')
+        self.save_config_json = params.get('save_config_json')
 
     def init(self, source_name):
         super(RawHitBuilder, self).init(source_name)
-        self.load_pedestals()
         self.load_configurations()
+        self.load_pedestals()
 
         # save all config info
         self.data_manager.set_attrs(self.hits_dset_name,
@@ -148,7 +151,7 @@ class RawHitBuilder(H5FlowStage):
             zy = resources['Geometry'].pixel_coordinates_2D[packets_arr['io_group'],
                                                 packets_arr['io_channel'], packets_arr['chip_id'], packets_arr['channel_id']]
             tile_id = resources['Geometry'].tile_id[packets_arr['io_group'],packets_arr['io_channel']]
-            print(min(tile_id), max(tile_id))
+            #print(min(tile_id), max(tile_id))
             x = resources['Geometry'].anode_drift_coordinate[(tile_id,)]
 
             raw_hits_arr['id'] = raw_hits_slice.start + np.arange(n, dtype=int)
@@ -174,15 +177,101 @@ class RawHitBuilder(H5FlowStage):
     @staticmethod
     def charge_from_dataword(dw, vref, vcm, ped):
         return dw / 256. * (vref - vcm) + vcm - ped
-
+    
+    @staticmethod
+    def dac2mv(dac, vdda, bits=8):
+        return vdda * dac/(2**bits)
+    
+    @staticmethod
+    def adc2mv(adc, ref, cm, bits=8):
+        return (ref-cm) * adc/(2**bits) + cm
+    
     def load_pedestals(self):
         if self.pedestal_file != '' and not resources['RunData'].is_mc:
-            with open(self.pedestal_file, 'r') as infile:
-                for key, value in json.load(infile).items():
-                    self.pedestal[key] = value
+            if self.pedestal_file.split('.')[-1] == 'json':
+                # load pedestals from json file
+                with open(self.pedestal_file, 'r') as infile:
+                    for key, value in json.load(infile).items():
+                        self.pedestal[key] = value
+            elif self.pedestal_file.split('.')[-1] == 'h5' or self.pedestal_file.split('.')[-1] == 'hdf5':
+                # Generate channel by channel pedestals from pedestal run h5 file
+                # Based on script at https://github.com/larpix/larpix-v2-testing-scripts/blob/master/event-display/gen_pedestal_json.py
+                print(f'Generating pedestals from {self.pedestal_file}')
+                mean_trunc = 3
+                f_ped = h5py.File(self.pedestal_file,'r')
+                ped_packets = np.array(f_ped['packets'])
+                ped_packets = ped_packets[(ped_packets['valid_parity'] == 1) & (ped_packets['packet_type'] == 0)]
+                unique_id = ((ped_packets['io_group'].astype(int)*256 \
+                    + ped_packets['io_channel'].astype(int))*256 \
+                    + ped_packets['chip_id'].astype(int))*64 \
+                    + ped_packets['channel_id'].astype(int)
+                unique_id_sort = np.argsort(unique_id)
+                ped_packets[:] = ped_packets[unique_id_sort]
+                unique_id = unique_id[unique_id_sort]
+                
+                # find start and stop indices for each occurrance of a unique id.
+                # this helps to make generating pedestals much faster.
+                unique_id_set, start_indices = np.unique(unique_id, return_index=True)
+                end_indices = np.roll(start_indices, shift=-1)
+                end_indices[-1] = len(ped_packets) - 1
+    
+                unique_id_chunk_indices = {}
+                for val, start_idx, end_idx in zip(unique_id_set, start_indices, end_indices):
+                    unique_id_chunk_indices[val] = (start_idx, end_idx)
+                
+                # loop through each channel and calculate mean pedestal in mV
+                for unique in unique_id_set:
+                    vcm = self.configuration[unique]['vcm_mv']
+                    vref = self.configuration[unique]['vref_mv']
+                
+                    start_index, stop_index = unique_id_chunk_indices[unique]
+                    adcs = ped_packets['dataword'][start_index:stop_index]
+                    if len(adcs) < 1:
+                        continue
+                    vals,bins = np.histogram(adcs,bins=np.arange(257))
+                    peak_bin = np.argmax(vals)
+                    min_idx,max_idx = max(peak_bin-mean_trunc,0), min(peak_bin+mean_trunc,len(vals))
+                    ped_adc = np.average(bins[min_idx:max_idx]+0.5, weights=vals[min_idx:max_idx])
 
+                    self.pedestal[str(unique)] = dict(
+                        pedestal_mv=float(self.adc2mv(ped_adc,vref,vcm))
+                    )
+                f_ped.close()
+                ped_packets=0
+                if self.save_ped_json:
+                    with open(resources['Geometry'].det_geometry_file.split('/')[-1].strip('.yaml') + '_evd_ped.json', 'w') as fo:
+                        json.dump(self.pedestal, fo, sort_keys=True, indent=4)
+                
     def load_configurations(self):
         if self.configuration_file != '' and not resources['RunData'].is_mc:
+            # Load configuration from json file
             with open(self.configuration_file, 'r') as infile:
                 for key, value in json.load(infile).items():
                     self.configuration[key] = value
+        elif self.configuration_file == '' and not resources['RunData'].is_mc:
+            # Calculate configuration values and make dictionary on the fly
+            module_to_io_groups = resources['Geometry'].det_geometry_yaml['module_to_io_groups']
+            io_group_to_module = {value: key for key, values in module_to_io_groups.items() for value in values}
+            module_to_vcm_dac = resources['Geometry'].det_geometry_yaml['module_to_vcm_dac']
+            module_to_vref_dac = resources['Geometry'].det_geometry_yaml['module_to_vref_dac']
+            module_to_vdda = resources['Geometry'].det_geometry_yaml['module_to_vdda']
+            io_groups = resources['Geometry'].pixel_coordinates_2D.keys()[0]
+            unique = ((resources['Geometry'].pixel_coordinates_2D.keys()[0]*256 \
+                    + resources['Geometry'].pixel_coordinates_2D.keys()[1])*256 \
+                    + resources['Geometry'].pixel_coordinates_2D.keys()[2])*64 \
+                    + resources['Geometry'].pixel_coordinates_2D.keys()[3]
+            # loop through the channels
+            for i in range(len(resources['Geometry'].pixel_coordinates_2D.keys()[0])):
+                module_id = io_group_to_module[io_groups[i]]
+                vcm_dac = module_to_vcm_dac[module_id]
+                vref_dac = module_to_vref_dac[module_id]
+                vdda = module_to_vdda[module_id]
+                self.configuration[str(unique[i])] = dict(
+                    vref_mv=float(self.dac2mv(vref_dac, vdda)),
+                    vcm_mv=float(self.dac2mv(vcm_dac, vdda)),
+                    vref_dac=int(vref_dac),
+                    vcm_dac=int(vcm_dac)
+                    )
+            if self.save_config_json:
+                with open(resources['Geometry'].det_geometry_file.split('/')[-1].strip('.yaml') + '_evd_config.json', 'w') as fo:
+                    json.dump(self.configuration, fo, sort_keys=True, indent=4)
