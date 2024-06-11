19bd5c86beed28ed499cdd960a0ab6555b638064
Matt Kramer <mjkramer@users.noreply.github.com>
2024-06-10 21:14:01 -0700

    tmp modify default vref and vcm

--- src/proto_nd_flow/reco/charge/calib_prompt_hits.py
+++ src/proto_nd_flow/reco/charge/calib_prompt_hits.py
@@ -68,8 +68,8 @@ class CalibHitBuilder(H5FlowStage):
 
     #: ASIC ADC configuration lookup table
     configuration = defaultdict(lambda: dict(
-        vref_mv=1300,
-        vcm_mv=288
+        vref_mv=1568.0,
+        vcm_mv=478.1
     ))
 
     #: pixel pedestal value
