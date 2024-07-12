df9341f35a8c306ead7190dd7adc00e5750cfbb1
Matt Kramer <mjkramer@users.noreply.github.com>
2024-07-12 15:17:01 -0700

    Use Andrew's fork of pylandau
    Fixes build issues on Perlmutter

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setuptools.setup(name='module0_flow',
                      'scikit-image',
                      'scikit-learn',
                      'h5flow>=0.2.0',
-                     'pylandau',
+                     'pylandau @ git+https://github.com/cuddandr/pylandau.git#egg=pylandau',
                  ]
                  )
