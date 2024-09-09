43cc511bea3e3a553463c2a6d29eeb65f49ee4bd
Alexander Booth <abooth@login18.chn.perlmutter.nersc.gov>
2024-09-09 10:05:22 -0700

    Update tag of larnd-sim in input getting script.

--- scripts/ndlar_scripts/get_ndlar_input.sh
+++ scripts/ndlar_scripts/get_ndlar_input.sh
@@ -12,7 +12,7 @@ cd ${DATA_DIR}
 curl -O https://raw.githubusercontent.com/DUNE/larnd-sim/develop/larndsim/pixel_layouts/multi_tile_layout-3.0.40.yaml
 
 # ndlar detector description
-curl -O https://raw.githubusercontent.com/DUNE/larnd-sim/develop/larndsim/detector_properties/ndlar-module.yaml
+curl -O https://raw.githubusercontent.com/DUNE/larnd-sim/nd-production-v02.00/larndsim/detector_properties/ndlar-module.yaml
 
 # Placeholder
 curl -O https://portal.nersc.gov/project/dune/data/2x2/simulation/kwood_dev/proto_nd_flow_inputs/runlist-2x2-mcexample.txt
