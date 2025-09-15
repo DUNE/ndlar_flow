95fb9fe046da9906d03c5ab3e9bd1778f55de5f2
Angela White <123964490+AWh1t3@users.noreply.github.com>
2025-09-15 09:31:55 -0500

    Update spe_inputs.sh

--- LRS_calibration_scripts/spe_inputs.sh
+++ LRS_calibration_scripts/spe_inputs.sh
@@ -4,16 +4,24 @@
 module unload python 2>/dev/null
 module load python/3.11
 
+# This is the waveform file you built earlier:
+
 inFile=/global/cfs/cdirs/dune/users/ajwhite/2x2_LRS_DataAssess/2025_Calibration/FiltFiles_v03_wvfms.npz
 #inFile=/global/cfs/cdirs/dune/users/ajwhite/2x2_LRS_DataAssess/2025_Calibration/FSD_DC_FiltFiles_v01_wvfms.npz
 
+# This is a pdf output with the validation plots
 outFile=/global/cfs/cdirs/dune/users/ajwhite/2x2_LRS_DataAssess/2025_Calibration/AFIViewer/Output_07/FiltChans_Amp_v04_3plots.pdf
 #outFile=/global/cfs/cdirs/dune/users/ajwhite/2x2_LRS_DataAssess/2025_Calibration/AFIViewer/Output_FSD_01/FSD_DC_FiltChans_PDF_v01.pdf
 
 echo "Starting Run Attempt"
 
-#python3 sipm_spe_analysis_v2.py --i $inFile --o $outFile
-python3 sipm_spe_amplitude.py --i $inFile --o $outFile
+# 2x2:
+python3 sipm_spe_analysis_v2.py --i $inFile --o $outFile
+
+# just a test:
+#python3 sipm_spe_amplitude.py --i $inFile --o $outFile
+
+# FSD:
 #python3 sipm_spe_analysis_FSD.py --i $inFile --o $outFile
         
 echo "Finished Run Attempt"
