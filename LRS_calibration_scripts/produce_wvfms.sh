3bbf4fae6cb6abcc899fda6d6c347b9aaa395428
Angela White <123964490+AWh1t3@users.noreply.github.com>
2025-09-15 09:30:11 -0500

    Update produce_wvfms.sh

--- LRS_calibration_scripts/produce_wvfms.sh
+++ LRS_calibration_scripts/produce_wvfms.sh
@@ -6,12 +6,15 @@ module load python/3.11
 
 #N_files=200
 
+# 2x2: you need the file location for your output (which is isolated noise-only and darkcount pulse waveforms)
 #output_file=/global/cfs/cdirs/dune/users/ajwhite/2x2_LRS_DataAssess/2025_Calibration/FiltFiles_v03_wvfms.npz
-output_file=/global/cfs/cdirs/dune/users/ajwhite/2x2_LRS_DataAssess/2025_Calibration/FSD_DC_FiltFiles_v01_wvfms.npz
+# FSD: 
+#output_file=/global/cfs/cdirs/dune/users/ajwhite/2x2_LRS_DataAssess/2025_Calibration/FSD_DC_FiltFiles_v01_wvfms.npz
 
 echo "Starting Run Attempt"
-
-#python3 Step2_pythonScript.py --o $output_file
-python3 Step3_FSD_pythonScript.py --o $output_file
+#2x2, run this from the directory where your script lives
+python3 Step2_pythonScript.py --o $output_file
+FSD
+#python3 Step3_FSD_pythonScript.py --o $output_file
         
 echo "Finished Run Attempt"
