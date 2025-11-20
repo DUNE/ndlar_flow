9d6a964b4223de5c89b28c3175143f03107051c2
James Vincent Mead <jmead@nikhef.nl>
2025-11-20 03:51:57 -0800

    sanity check run

--- src/proto_nd_flow/reco/light/wvfm_noise_filter.py
+++ src/proto_nd_flow/reco/light/wvfm_noise_filter.py
@@ -55,7 +55,7 @@ class WaveformNoiseFilter(H5FlowStage):
     default_segment_size = 25
     default_num_segment = 40
     default_noise_dset_name = 'light/fwvfm_noise'
-    
+
 
     def fwvfm_dtype(self, nadc, nchannels, nsamples): return np.dtype([('samples', 'f4', (nadc, nchannels, nsamples))])
 
@@ -71,7 +71,7 @@ class WaveformNoiseFilter(H5FlowStage):
         self.noise_dset_name = params.get('noise_dset_name', self.default_noise_dset_name)
         self.segment_size = params.get("segment_size", self.default_segment_size)
         self.num_segment = params.get("num_segment", self.default_num_segment)
-        
+
     def init(self, source_name):
         super(WaveformNoiseFilter, self).init(source_name)
 
@@ -94,32 +94,32 @@ class WaveformNoiseFilter(H5FlowStage):
             self.data_manager.create_dset(self.noise_dset_name, dtype=wvfm_dset.dtype)
             self.data_manager.create_ref(source_name, self.noise_dset_name)
 
-    def min_range_baseline(self, array, segment_size=25, num_segments=40):
+    def min_range_baseline(self, array, segment_size=25, num_segments=24):
 
         # Define start and end indices for segments
         indices = np.arange(num_segments + 1) * segment_size  # (41,)
         start_indices, end_indices = indices[:-1], indices[1:]  # (40,)
-    
+
         # Generate index array for advanced indexing
         segment_range = np.arange(segment_size)  # (25,)
         index_array = start_indices[:, None] + segment_range  # Shape: (40, 25)
-    
+
         # Extract data from segments using indexing
         sliced_data = array[..., index_array]  # Shape (..., 40, 25)
-    
+
         # Compute range (peak-to-peak difference) and mean for each segment
         ranges = np.ptp(sliced_data, axis=-1)  # Shape (..., 40)
         means = np.mean(sliced_data, axis=-1)  # Shape (..., 40)
-    
+
         # Find the ordering of the segments based on the smallest range
         smallest_ordering = np.argsort(ranges, axis=-1)  # Shape (..., 40)
-    
+
         # Sort means according to the ordering of smallest ranges
         sorted_means = np.take_along_axis(means, smallest_ordering, axis=-1)  # Shape (..., 40)
-    
+
         # Compute the average of the 2nd, 3rd, and 4th smallest means
         average_mean = np.mean(sorted_means[..., 1:4], axis=-1)  # Shape (...)
-    
+
         return average_mean
 
     def run(self, source_name, source_slice, cache):
