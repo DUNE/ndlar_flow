6fc0327dc3da1cb45d2259b6d5f6511ab7f75700
Noe Roy <noeroy@yorku.ca>
2025-12-11 08:58:13 -0800

    curl tables from database

--- scripts/proto_nd_scripts/get_proto_nd_input.sh
+++ scripts/proto_nd_scripts/get_proto_nd_input.sh
@@ -2,6 +2,7 @@
 
 #DATA_DIR=$1
 DATA_DIR="../../data/proto_nd_flow/"
+DATA_DIR_FSD="../../data/fsd_flow/"
 
 HERE=`pwd`
 
@@ -20,5 +21,126 @@ curl -O https://raw.githubusercontent.com/DUNE/larnd-sim/MiniRun6-v1/larndsim/de
 
 # place holder for light system geometry description
 curl -O https://portal.nersc.gov/project/dune/data/2x2/simulation/kwood_dev/proto_nd_flow_inputs/light_module_desc-0.0.0.yaml
+
+#Download 2x2 and FSD databases
+URL="https://dbdata0vm.fnal.gov:9443/dune_runcon_prod/get"
+
+
+echo "Querying 2x2 electron lifetime data..."
+curl -s "$URL" \
+   --get \
+   --data-urlencode "folder=neardet2x2.elifetime" \
+   --data-urlencode "t0=0" \
+   --data-urlencode "t1=1000000000000" \
+   --data-urlencode "data_type=2x2_data" \
+   --data-urlencode "format=json" \
+ | jq '
+     .rows
+     | map({ (.tv|tostring): [ .elifetime, .err_elifetime ] })
+     | add
+ ' > 2x2_electron_lifetime_db.json
+
+
+
+echo "Querying 2x2 pedestal data..."
+
+curl -s "$URL" \
+  --get \
+  --data-urlencode "folder=neardet2x2.pedestal_test" \
+  --data-urlencode "t0=0" \
+  --data-urlencode "t1=1000000000000" \
+  --data-urlencode "data_type=2x2_data" \
+  --data-urlencode "format=json" \
+| jq '
+  .rows
+  | group_by(.tv)
+  | map({
+      (.[0].tv|tostring):
+        ( map({ (.channel|tostring): { "pedestal_mv": .pedestal } }) | add )
+    })
+  | add
+' > 2x2_pedestal_db.json
+
+
+echo "Querying 2x2 gain data..."
+
+curl -s "$URL" \
+  --get \
+  --data-urlencode "folder=neardet2x2.gain" \
+  --data-urlencode "t0=0" \
+  --data-urlencode "t1=1000000000000" \
+  --data-urlencode "data_type=2x2_data" \
+  --data-urlencode "format=json" \
+| jq '
+  .rows
+  | group_by(.tv)
+  | map({
+      (.[0].tv|tostring):
+        ( map({ (.channel|tostring): { "gain": .pedestal } }) | add )
+    })
+  | add
+' > 2x2_gain_db.json
+
+
+
+
+
+cd $DATA_DIR_FSD
+
+
+echo "Querying FSD electron lifetime data..."
+curl -s "$URL" \
+  --get \
+  --data-urlencode "folder=neardet2x2.elifetime" \
+  --data-urlencode "t0=0" \
+  --data-urlencode "t1=1000000000000" \
+  --data-urlencode "data_type=FSD_data" \
+  --data-urlencode "format=json" \
+| jq '
+    .rows
+    | map({ (.tv|tostring): [ .elifetime, .err_elifetime ] })
+    | add
+' > FSD_electron_lifetime_db.json
+
+
+
+echo "Querying FSD pedestal data..."
+
+curl -s "$URL" \
+  --get \
+  --data-urlencode "folder=neardet2x2.pedestal_test" \
+  --data-urlencode "t0=0" \
+  --data-urlencode "t1=1000000000000" \
+  --data-urlencode "data_type=FSD_data" \
+  --data-urlencode "format=json" \
+| jq '
+  .rows
+  | group_by(.tv)
+  | map({
+      (.[0].tv|tostring):
+        ( map({ (.channel|tostring): { "pedestal_mv": .pedestal } }) | add )
+    })
+  | add
+' > FSD_pedestal_db.json
+
+
+echo "Querying FSD gain data..."
+
+curl -s "$URL" \
+  --get \
+  --data-urlencode "folder=neardet2x2.gain" \
+  --data-urlencode "t0=0" \
+  --data-urlencode "t1=1000000000000" \
+  --data-urlencode "data_type=FSD_data" \
+  --data-urlencode "format=json" \
+| jq '
+  .rows
+  | group_by(.tv)
+  | map({
+      (.[0].tv|tostring):
+        ( map({ (.channel|tostring): { "gain": .pedestal } }) | add )
+    })
+  | add
+' > FSD_gain_db.json
 
 cd ${HERE}
