f1eb7cbf6426eb8e6f30c15f1e0bb339de039e76
Matt Kramer <mjkramer@users.noreply.github.com>
2026-01-10 12:11:51 -0800

    Fix MPD event gen for latest numpy

--- src/proto_nd_flow/reco/light/mpd_event_generator.py
+++ src/proto_nd_flow/reco/light/mpd_event_generator.py
@@ -205,14 +205,14 @@ class LightMPDEventGenerator(H5FlowGenerator):
                 data = [np.array(events['data'][index]) for index in self.valid_adc_index]
                 device = np.array([events['device'][index] for index in self.valid_adc_index])
                 time = np.array([events['time'][index] for index in self.valid_adc_index])
-                event_arr[ievent]['event'] = event['event']
+                event_arr[ievent]['event'] = event['event'][0]
                 for iadc, sn in enumerate(self.sn_table):
                     data_index = np.where(device["serial"] == sn)[0]
                     if len(data_index):
                         channels = data[data_index.item()]['channel']
-                        event_arr[ievent]['sn'][iadc] = device[data_index.item()]['serial']
-                        event_arr[ievent]['utime_ms'][iadc] = event['unix_ms']
-                        event_arr[ievent]['tai_ns'][iadc] = time[data_index.item()]['tai_s']*1e9 + time[data_index.item()]['tai_ns']
+                        event_arr[ievent]['sn'][iadc] = device[data_index.item()]['serial'][0]
+                        event_arr[ievent]['utime_ms'][iadc] = event['unix_ms'][0]
+                        event_arr[ievent]['tai_ns'][iadc] = time[data_index.item()]['tai_s'][0]*1e9 + time[data_index.item()]['tai_ns'][0]
                         event_arr[ievent]['wvfm_valid'][iadc, channels] = True
                         wvfm_arr[ievent]['samples'][iadc, channels] = data[data_index.item()]['voltage']
                     else:
