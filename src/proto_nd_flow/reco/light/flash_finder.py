ea4c04e52dc8b50f2c25dfaf194dd381aaba2073
sindhu-ku <kumaran.sindhujha@gmail.com>
2026-01-26 03:58:06 -0800

    revert to old flash finder

--- src/proto_nd_flow/reco/light/flash_finder.py
+++ src/proto_nd_flow/reco/light/flash_finder.py
@@ -37,8 +37,8 @@ class FlashFinder(H5FlowStage):
             ('tot_max', 'f4'),
             ('tot_sum_spline', 'f4'),
             ('tot_max_spline', 'f4'),
-            ('deconv_sum', 'f4', (2, nchantpc//2)),
-            ('deconv_max', 'f4', (2, nchantpc//2))
+            ('sum_pe_ch', 'f4', (2, nchantpc//2)),
+            ('max_pe_ch', 'f4', (2, nchantpc//2))
         ])
 
     def flash_sipm_dtype(self, nchantpc):
@@ -53,12 +53,14 @@ class FlashFinder(H5FlowStage):
             ('tot_max', 'f4'),
             ('tot_sum_spline', 'f4'),
             ('tot_max_spline', 'f4'),
-            ('sum_pe_ch', 'f4',  (2, nchantpc//2)),
-            ('max_pe_ch', 'f4',  (2, nchantpc//2))
+            ('sum_pe_ch', 'f4', (2, nchantpc//2)),
+            ('max_pe_ch', 'f4', (2, nchantpc//2))
         ])
 
     def __init__(self, **params):
-        super().__init__(**params)
+        super(FlashFinder, self).__init__(**params)
+        
+        # set up parameters
         for key, val in self.defaults.items():
             setattr(self, key, params.get(key, val))
 
@@ -66,23 +68,26 @@ class FlashFinder(H5FlowStage):
         self.flash_sipm_dtype = self.flash_sipm_dtype(self.nchantpc)
 
     def init(self, source_name):
-        super().init(source_name)
+        super(FlashFinder, self).init(source_name)
 
         cwvfm_dset = self.data_manager.get_dset(self.cwvfm_dset_name)
         self.sum_hits_dset = self.data_manager.get_dset(self.sum_hits_dset_name)
         self.sipm_hits_dset = self.data_manager.get_dset(self.sipm_hits_dset_name)
 
         self.dbs = cluster.DBSCAN(eps=self.eps, min_samples=self.min_samples)
-
+        
+        # get waveform shape information
         self.nadc = cwvfm_dset.dtype['samples'].shape[0]
         self.nchan = cwvfm_dset.dtype['samples'].shape[1]
         self.ntpc = self.nadc
         self.nsamples = cwvfm_dset.dtype['samples'].shape[2]
-
+        
+        # Load channel map
         self.rel_pos_map = np.zeros((self.nadc, self.nchan, 3))
         for adc in range(self.nadc):
-            self.rel_pos_map[adc, :, :] = resources['Geometry'].sipm_rel_pos[(adc, range(64))]
-
+            self.rel_pos_map[adc, :, :] = resources['Geometry'].sipm_rel_pos[(adc, range(self.nchan))]
+        
+        # create datasets and references
         self.data_manager.create_dset(self.flash_dset_name, dtype=self.flash_dtype)
         self.data_manager.create_dset(self.flash_sipm_dset_name, dtype=self.flash_sipm_dtype)
 
@@ -90,33 +95,49 @@ class FlashFinder(H5FlowStage):
         self.data_manager.create_ref(source_name, self.flash_sipm_dset_name)
         self.data_manager.create_ref(self.sum_hits_dset_name, self.flash_dset_name)
         self.data_manager.create_ref(self.sipm_hits_dset_name, self.flash_sipm_dset_name)
+        self.data_manager.set_attrs(self.flash_dset_name,
+                                    classname=self.classname,
+                                    class_version=self.class_version,
+                                    sum_hits_dset=self.sum_hits_dset_name,
+                                    sipm_hits_dset=self.sipm_hits_dset_name
+                                   )
 
+    def get_tpc_channels(self,itpc):
+        '''
+        Returns array with (adc,channel) indices for given TPC
+        The returnd array has size (2,nchantpc//2,2) where
+        the first index is the side of the tpc (looking from cathode to anode, 0:left,1:right)
+        and the second is the vertical position from bottom to top.
+        The third axis is the index for adc (0) or channel (1).
+        '''
+        return_arr = np.zeros((2,self.nchantpc//2,2),dtype='i2')
+        for iside in range(2):
+            indices = np.where((self.rel_pos_map[..., 0] == itpc) & (self.rel_pos_map[..., 1] == iside))
+            indexed_values = list(zip(indices[0], indices[1]))
+            indexed_values.sort(key=lambda x: self.rel_pos_map[x[0], x[1], 2])
+            return_arr[iside,:,:] = np.array(indexed_values)
+
+        return(return_arr)
+                                        
     def get_tpc_mask(self, adc, chan, itpc):
         return (self.rel_pos_map[adc, chan, 0] == itpc)
 
     def get_extrema(self, input_array):
         return np.array([input_array.min(), input_array.max()])
-    
-    def get_tpc_channels(self, itpc):
-        return_arr = np.zeros((2, self.nchantpc//2, 2), dtype='i2')
-        for iside in range(2):
-            indices = np.where((self.rel_pos_map[..., 0] == itpc) & (self.rel_pos_map[..., 1] == iside))
-            indexed = list(zip(indices[0], indices[1]))
-            indexed.sort(key=lambda x: self.rel_pos_map[x[0], x[1], 2])
-            return_arr[iside, :, :] = np.array(indexed)
-        return return_arr
         
     def run(self, source_name, source_slice, cache):
-        super().run(source_name, source_slice, cache)
+        super(FlashFinder, self).run(source_name, source_slice, cache)
         events = cache[source_name]
         cwvfms = cache[self.cwvfm_dset_name].reshape(events.shape)['samples']
-
+     
+        #Get assosciate hits for events slice
         sum_hits, sum_hits_idx = self._load_hit_data(source_name, source_slice, self.sum_hits_dset_name, self.sum_hits_dset)
         sipm_hits, sipm_hits_idx = self._load_hit_data(source_name, source_slice, self.sipm_hits_dset_name, self.sipm_hits_dset)
 
         sum_flash_list, sum_ev_ref_list, sum_hit_ref_list = [], [], []
         sipm_flash_list, sipm_ev_ref_list, sipm_hit_ref_list = [], [], []
-
+      
+        if VERBOSE: print("# events in slice: ",len(events))
         for i, ev in enumerate(events):
             for itpc in range(self.ntpc):
                 # Sum Hit flashes
@@ -146,12 +167,21 @@ class FlashFinder(H5FlowStage):
         if not len(tpc_hits): return
         labels = self.dbs.fit_predict(tpc_hits['sample_idx'].reshape(-1, 1))
         n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+        # skip if there's no light clusters
         if n_clusters == 0: return
 
         flashes = np.empty(n_clusters, dtype=self.flash_dtype)
         ev_ref = np.full(n_clusters, np.r_[source_slice][i], dtype='u4')
         sum_ref = np.empty((len(tpc_hits_idx), 2), dtype='u4')
 
+        if VERBOSE:
+            print("    TPC #",itpc," #Clusters ",n_clusters)
+            print("       #Sum Hits    ",sum_hits[i,mask].shape)
+            print("       Sum Hits:    ",sum_hits[i,mask]["sample_idx"])
+            print("       Sum Hits IDs:    ",sum_hits[i,mask]["id"])
+            print("       Labels:  ",labels)
+        #Handle clusters
+        # Note: Clusters pre-sorted in time by DBSCAN
         for cl in range(n_clusters):
             cl_hits = tpc_hits[labels == cl]
             flashes[cl]['id'] = 0
@@ -167,8 +197,8 @@ class FlashFinder(H5FlowStage):
 
             ch_idx = self.get_tpc_channels(itpc)
             flash_slice = slice(flashes[cl]['sample_range'][0], flashes[cl]['sample_range'][1] + 1)
-            flashes[cl]['deconv_sum'] = np.sum(cwvfms[i, ch_idx[..., 0], ch_idx[..., 1], flash_slice], axis=-1)
-            flashes[cl]['deconv_max'] = np.max(cwvfms[i, ch_idx[..., 0], ch_idx[..., 1], flash_slice], axis=-1)
+            flashes[cl]['sum_pe_ch'] = np.sum(cwvfms[i, ch_idx[..., 0], ch_idx[..., 1], flash_slice], axis=-1)
+            flashes[cl]['max_pe_ch'] = np.max(cwvfms[i, ch_idx[..., 0], ch_idx[..., 1], flash_slice], axis=-1)
 
         sum_ref[:, 0] = tpc_hits_idx[labels >= 0]
         sum_ref[:, 1] = labels[labels >= 0]
@@ -184,23 +214,38 @@ class FlashFinder(H5FlowStage):
 
         labels = self.dbs.fit_predict(tpc_hits['sample_idx'].reshape(-1, 1))
         n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+        # skip if there's no light hits
         if n_clusters == 0: return
 
         flashes = np.empty(n_clusters, dtype=self.flash_sipm_dtype)
         ev_ref = np.full(n_clusters, np.r_[source_slice][i], dtype='u4')
         hit_ref = np.empty((len(tpc_hits_idx), 2), dtype='u4')
         self.tpc_chan_to_local = dict()
-        chan_counter = defaultdict(lambda: [0, 0])  # [side0_count, side1_count]
-
-        for adc in range(self.rel_pos_map.shape[0]):
-            for chan in range(self.rel_pos_map.shape[1]):
-                tpc = int(self.rel_pos_map[adc, chan, 0])
-                side = int(self.rel_pos_map[adc, chan, 1])
-                if tpc >= 0 and side in [0, 1]:
-                    local_idx = chan_counter[tpc][side]
-                    self.tpc_chan_to_local[(tpc, side, adc, chan)] = (side, local_idx)
-                    chan_counter[tpc][side] += 1
-
+        chan_counter = defaultdict(int)  # counts channels per (tpc, side)
+        
+        # for adc in range(self.rel_pos_map.shape[0]):
+        #     for chan in range(self.rel_pos_map.shape[1]):
+        #         tpc = int(self.rel_pos_map[adc, chan, 0])
+        #         side = int(self.rel_pos_map[adc, chan, 1])
+        #         pos =  int(self.rel_pos_map[adc, chan, 2])
+        #         y = resources['Geometry'].sipm_abs_pos[(adc, chan)][0][1]
+        #         if tpc < 0:
+        #             continue
+        #         local_idx = chan_counter[(tpc, side)]  # integer count
+        #         print(tpc, side, pos, resources['Geometry'].sipm_abs_pos[(adc, chan)], adc, chan, local_idx)
+        #         self.tpc_chan_to_local[(tpc, side, y, adc, chan)] = (side, local_idx)
+        
+        #         chan_counter[(tpc, side)] += 1
+      
+        if VERBOSE:
+            print("    TPC #",itpc," #Clusters ",n_clusters)
+            print("       #SiPM Hits    ",sipm_hits[i,mask].shape)
+            print("       SiPM Hits:    ",sipm_hits[i,mask]["sample_idx"])
+            print("       SiPM Hits IDs:    ",sipm_hits[i,mask]["id"])
+            print("       Labels:  ",labels)
+        
+         #Handle clusters
+        # Note: Clusters pre-sorted in time by DBSCAN
         for cl in range(n_clusters):
             cl_hits = tpc_hits[labels == cl]
             flashes[cl]['id'] = 0
@@ -212,10 +257,61 @@ class FlashFinder(H5FlowStage):
             flashes[cl]['tot_sum'] = cl_hits['sum'].sum()
             flashes[cl]['tot_max'] = cl_hits['max'].sum()
             flashes[cl]['tot_sum_spline'] = cl_hits['sum_spline'].sum()
-            flashes[cl]['tot_max_spline'] = cl_hits['max_spline'].sum
+            flashes[cl]['tot_max_spline'] = cl_hits['max_spline'].sum()
             
-            #use sipm_hits directly to get pe
+            #get pe per sipm information
+            sum_pe = np.zeros((2, self.nchantpc // 2))
+            max_pe = np.zeros((2, self.nchantpc // 2))
             
+            # flash_slice = slice(flashes[cl]['sample_range'][0], flashes[cl]['sample_range'][1] + 1)
+            
+            # for h in cl_hits:
+            #     adc = h['adc']
+            #     chan = h['chan']
+            #     tpc = int(self.rel_pos_map[adc, chan, 0])
+            #     side = int(self.rel_pos_map[adc, chan, 1])
+            
+            #     side_idx, local_idx = self.tpc_chan_to_local[(tpc, side, adc, chan)]
+            #     wf = cwvfms[i, adc, chan, flash_slice]
+            
+            #     sum_pe[side_idx, local_idx] = np.sum(wf)
+            #     max_pe[side_idx, local_idx] = np.max(wf)
+            
+            # flashes[cl]['sum_pe_ch'] = sum_pe
+            # flashes[cl]['max_pe_ch'] = max_pe
+           # Step 1: get normal channel index array
+            # ch_idx = self.get_tpc_channels(itpc)  # shape: (2, nchan_per_side, 2)
+            
+            # flash_slice = slice(flashes[cl]['sample_range'][0], flashes[cl]['sample_range'][1] + 1)
+            # adc_arr = sipm_hits[i]['adc']
+            # chan_arr = sipm_hits[i]['chan']
+            
+            # # Masked arrays: filter only valid entries (non-masked)
+            # valid_mask = ~(ma.getmaskarray(adc_arr) | ma.getmaskarray(chan_arr))
+            
+            # valid_adc = adc_arr[valid_mask]
+            # valid_chan = chan_arr[valid_mask]
+            
+            # valid_pairs = set(zip(valid_adc, valid_chan))
+           
+            # # Step 3: make a boolean mask with same shape as first two dims of ch_idx
+            # ch_mask = np.array(
+            #     [[(adc, chan) in valid_pairs for adc, chan in row] for row in ch_idx],
+            #     dtype=bool
+            # )
+            
+            # # Step 4: get sums/max as usual
+            # sum_pe_ch = np.sum(cwvfms[i, ch_idx[..., 0], ch_idx[..., 1], flash_slice], axis=-1)
+            # max_pe_ch = np.max(cwvfms[i, ch_idx[..., 0], ch_idx[..., 1], flash_slice], axis=-1)
+            
+            # # Step 5: zero out channels not in sipm_hits
+            # sum_pe_ch[~ch_mask] = 0
+            # max_pe_ch[~ch_mask] = 0
+
+
+
+            # Step 6: store
+ 
             # sum_pe = np.zeros((2, self.nchantpc // 2))
             # max_pe = np.zeros((2, self.nchantpc // 2))
             
@@ -234,15 +330,39 @@ class FlashFinder(H5FlowStage):
             #     sum_pe[side_idx, local_idx] += h['sum']
             #     max_pe[side_idx, local_idx] += h['max']
             
-            # flashes[cl]['sum_pe_ch'] = sum_pe
-            # flashes[cl]['max_pe_ch'] = max_pe
+            # # flashes[cl]['sum_pe_ch'] = sum_pe
+            # # flashes[cl]['max_pe_ch'] = max_pe
 
-            #use the same method as sum hits to get pe
+
             ch_idx = self.get_tpc_channels(itpc)
             flash_slice = slice(flashes[cl]['sample_range'][0], flashes[cl]['sample_range'][1] + 1)
-            flashes[cl]['sum_pe_ch'] = np.sum(cwvfms[i, ch_idx[..., 0], ch_idx[..., 1], flash_slice], axis=-1)
-            flashes[cl]['max_pe_ch'] = np.max(cwvfms[i, ch_idx[..., 0], ch_idx[..., 1], flash_slice], axis=-1)
+            
+            # Shape: (2, nchans_per_side)
+            sum_pe = np.sum(cwvfms[i, ch_idx[..., 0], ch_idx[..., 1], flash_slice], axis=-1)
+            max_pe = np.max(cwvfms[i, ch_idx[..., 0], ch_idx[..., 1], flash_slice], axis=-1)
+            
+        
+            # Get ordering for each side based on Y position
+            for side in (0, 1):
+                # Extract adc and chan for this side
+                adc_side = ch_idx[side, :, 0]
+                chan_side = ch_idx[side, :, 1]
+            
+                # Get Y positions
+                y_pos = np.array([resources['Geometry'].sipm_abs_pos[(adc, chan)][0][1]
+                                  for adc, chan in zip(adc_side, chan_side)])
+                if itpc==0: print(y_pos)
+                # Sort by Y (bottom to top)
+                order = np.argsort(y_pos)
+            
+                # Apply ordering
+                sum_pe[side] = sum_pe[side][order]
+                max_pe[side] = max_pe[side][order]
 
+            
+            flashes[cl]['sum_pe_ch'] = sum_pe
+            flashes[cl]['max_pe_ch'] = max_pe
+            # print(sum_pe, flashes[cl]['sum_pe_ch'])
         hit_ref[:, 0] = tpc_hits_idx[labels >= 0]
         hit_ref[:, 1] = labels[labels >= 0]
 
