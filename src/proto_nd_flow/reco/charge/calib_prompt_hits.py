09008ca654459227dc8b96454e570ced73db50fe
MadivB <67230306+MadivB@users.noreply.github.com>
2026-05-13 09:25:24 +0800

    Update calib_prompt_hits.py

--- src/proto_nd_flow/reco/charge/calib_prompt_hits.py
+++ src/proto_nd_flow/reco/charge/calib_prompt_hits.py
@@ -65,6 +65,7 @@ class CalibHitBuilder(H5FlowStage):
             Q_raw          f8, hit charge [ke-] (uncalibrated for ADC droop)
             Q              f8, calibrated hit charge with ADC droop corrections
             E              f8, hit energy [MeV]
+            is_disabled    ? , Missing definition FIXME 
             x_corr         f8, drift-time corrected x location [cm]
             t_0            i2, charge-light matching timestamp [ns]
             t_confidence   f4, charge-light matching accuracy confidence      
