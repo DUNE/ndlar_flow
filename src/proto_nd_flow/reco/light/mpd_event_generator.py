43a537e7092af53a60d773493dda8f68a2a77a56
nsallin <nicolas.sallin@unibe.ch>
2026-06-19 17:31:30 -0500

    fix indexing

--- src/proto_nd_flow/reco/light/mpd_event_generator.py
+++ src/proto_nd_flow/reco/light/mpd_event_generator.py
@@ -216,7 +216,7 @@ class LightMPDEventGenerator(H5FlowGenerator):
                             channels = np.arange(self.first_channel, self.first_channel + self.n_channels)
                         event_arr[ievent]['sn'][iadc] = device[data_index.item()]['serial'][0]
                         event_arr[ievent]['utime_ms'][iadc] = event['unix_ms'][0]
-                        event_arr[ievent]['tai_ns'][iadc] = time[data_index.item()]['tai_s']*1e9 + time[data_index.item()]['tai_ns'][0]
+                        event_arr[ievent]['tai_ns'][iadc] = time[data_index.item()]['tai_s'][0]*1e9 + time[data_index.item()]['tai_ns'][0]
                         event_arr[ievent]['wvfm_valid'][iadc, channels] = True
                         wvfm_arr[ievent]['samples'][iadc, channels] = data[data_index.item()]['voltage'][channels]
                     else:
